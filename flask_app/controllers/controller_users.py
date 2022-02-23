dc7a2a557eaf9325492df2d897cbb978e839add3
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-23 14:36:33 -0600

    Saving to DB First Time

--- flask_app/controllers/controller_users.py
+++ flask_app/controllers/controller_users.py
@@ -5,28 +5,35 @@ from flask_app.models.user import User
 from flask_app.models.group import Group
 
 
-from flask_bcrypt import Bcrypt        
-bcrypt = Bcrypt(app)   
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
 
 @app.route("/")
 def index_to_users():
+    print_location = "index_to_users"
+    User.print_location(print_location)
     return redirect("/users")
+
 
 @app.route("/users")
 def show_users():
+    print_location = "show_users"
+    User.print_location(print_location)
     return render_template("users.html")
 
 ####################
 
+
 @app.route("/users/fun_create_new", methods=["POST"])
 def fun_create_new():
-
+    print_location = "fun_create_new"
+    User.print_location(print_location)
     data = {
-        "name": request.form("user_name"),
-        "email": request.form("email"),
-        "password": request.form("password")
+        "user_name": request.form["user_name"],
+        "email": request.form["email"],
+        "password": request.form["password"]
     }
-
+    User.print_location("after data")
     user_id = User.save_user(data)
-
-    return user_id;
+    return user_id
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -16,7 +16,9 @@ class User:
         self.updated_at = data["updated_at"]
 
     @classmethod
-    def get_all(cls):
+    def get_all_users(cls):
+        print_location = "get_all_users"
+        User.print_location(print_location)
         query = "SELECT * FROM users;"
         results = connectToMySQL('users_and_groups').query_db(query)
         users = []
@@ -26,6 +28,16 @@ class User:
 
     @classmethod
     def save_user(cls, data):
-        query = "INSERT INTO users (name, password, email, created_at, updated_at) VALUES (%(user_name)s, %(password)s, %(email)s, NOW(), NOW());"
+        print_location = "save_user"
+        User.print_location(print_location)
+        query = "INSERT INTO users (name, email, password, created_at, updated_at) VALUES (%(user_name)s, %(email)s, %(password)s, NOW(), NOW());"
         new_id = connectToMySQL("users_and_groups").query_db(query, data)
         return new_id
+
+    ##########################
+
+    @staticmethod
+    def print_location(message):
+        print("------------------------")
+        print(f"{message}")
+        print("------------------------")
